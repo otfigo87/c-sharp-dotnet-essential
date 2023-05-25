7844036e401d76d5b2ff2b4eedcc94652360d57e
otmane aatik <aaotmane@gmail.com>
2023-05-25 17:31:30 -0400

    my solution

--- Start/NumbersDates/Challenge/program.cs
+++ Start/NumbersDates/Challenge/program.cs
@@ -1,6 +1,37 @@
+DateTime today = DateTime.Today;
 
+string thedate;
+
+DateTime parsedDate;
+
+TimeSpan ts;
+
+do
+{
+    Console.WriteLine("Enter your date");
+    thedate = Console.ReadLine();
+
+    if (DateTime.TryParse(thedate, out parsedDate))
+    {
+        if (today > parsedDate)
+        {
+            ts = today - parsedDate;
+            Console.WriteLine($"{ts.Days} days ago!");
+        }
+        else if (today < parsedDate)
+        {
+            ts = parsedDate - today;
+            Console.WriteLine($"{ts.Days} more days to go!");
+        }
+        else
+        {
+            Console.WriteLine("it's today!!!");
+        }
+
+    }
+    else
+    {
+        Console.WriteLine("can't get parsed");
+    }
 }
-
-
-
-
+while (thedate != "exit");
