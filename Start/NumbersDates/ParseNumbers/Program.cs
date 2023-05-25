7d42ead73a87c6541b5b3800f5091438e15b5044
otmane aatik <aaotmane@gmail.com>
2023-05-25 12:44:56 -0400

    Parseing

--- Start/NumbersDates/ParseNumbers/Program.cs
+++ Start/NumbersDates/ParseNumbers/Program.cs
@@ -10,8 +10,25 @@ bool result;
 // TODO: The Parse method attempts to parse a string to a number and
 // throws an exception if the parse is unsuccessful
 foreach (string str in NumStrs) {
+    try {
+        testint = int.Parse(str);
+        Console.WriteLine($"parsed number is {str}");
+    }catch(Exception){
+        Console.WriteLine("Could not Parse " + str);
+    }
 
 }
 
 // TODO: The TryParse method returns 'true' if the parse is successful
+
+result = int.TryParse(NumStrs[0], out testint);
+System.Console.WriteLine($"{result} -- {NumStrs[0]} : {testint}");
+
+result = float.TryParse(NumStrs[1], out testfloat);
+System.Console.WriteLine($"{result} -- {NumStrs[1]} : {testfloat}");
+
+result = int.TryParse(NumStrs[3], out testint);
+System.Console.WriteLine($"{result} -- {NumStrs[3]} : {testint}");
+
+
 
