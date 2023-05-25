7b53f49a3de4e019768e094a60b6c774228c656a
otmane aatik <aaotmane@gmail.com>
2023-05-25 14:11:12 -0400

    FormatDates

--- Start/NumbersDates/FormatDates/Program.cs
+++ Start/NumbersDates/FormatDates/Program.cs
@@ -5,24 +5,35 @@
 DateTime AprFools = new DateTime(2025, 4, 1, 13, 23, 30);
 
 // TODO: 'd' Short date: mm/dd/yyyy (or dd/mm depending on locale)
+System.Console.WriteLine($"{AprFools:d}");
 
 
 // TODO: 'D' Full date: mm/dd/yyyy (or dd/mm depending on locale)
+System.Console.WriteLine($"{AprFools:D}");
 
 
 // TODO: 'f' Full date, short time
+System.Console.WriteLine($"{AprFools:f}");
 
 
 // TODO: 'F' Full date, long time
+System.Console.WriteLine($"{AprFools:F}");
 
 
 // TODO: 'g' General date and time
+System.Console.WriteLine($"{AprFools:g}");
 
 
 // TODO: 'G' General date and time
+System.Console.WriteLine($"{AprFools:G}");
 
 
 // TODO: Format using a specific CultureInfo
+// Console.WriteLine(AprFools.ToString("d", CultureInfo.CreateSpecificCulture("de-DE")));
 
 
 // TODO: Defining custom date and time formats
+Console.WriteLine($"{AprFools:dddd, MMMM d yyyy}");
+Console.WriteLine($"{AprFools:ddd h:mm:ss tt}");
+Console.WriteLine($"{AprFools:MMM d yyyy}");
+
