cc61a00a414cd39bf35ec897893617a4dad6f137
otmane aatik <aaotmane@gmail.com>
2023-05-30 12:01:11 -0400

    file info

--- Start/Files/FileInfo/Program.cs
+++ Start/Files/FileInfo/Program.cs
@@ -11,10 +11,28 @@ if (!File.Exists(filename)) {
 }
 
 // TODO: Get some information about the file
+System.Console.WriteLine(File.GetCreationTime(filename));
+System.Console.WriteLine(File.GetLastWriteTime(filename));
+System.Console.WriteLine(File.GetLastAccessTime(filename));
+
+File.SetAttributes(filename, FileAttributes.ReadOnly);
+
+System.Console.WriteLine(File.GetAttributes(filename));
 
 
 // TODO: We can also get general information using a FileInfo 
+try {
+    FileInfo fi = new FileInfo(filename);
+    System.Console.WriteLine($"{fi.Length}");
+    System.Console.WriteLine(fi.Directory);
+    System.Console.WriteLine(fi.IsReadOnly);
+} catch (Exception e){
+    System.Console.WriteLine($"Exception: {e}");
+}
 
 
 // TODO: File information can also be manipulated
+DateTime dt = new DateTime(1990, 7, 1);
+File.SetCreationTime(filename, dt);
+System.Console.WriteLine(File.GetCreationTime(filename));
 
