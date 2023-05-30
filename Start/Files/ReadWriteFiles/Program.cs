91d6897ac32d8518606b6564e1ef559df21f840f
otmane aatik <aaotmane@gmail.com>
2023-05-30 11:43:45 -0400

    read/write files

--- Start/Files/ReadWriteFiles/Program.cs
+++ Start/Files/ReadWriteFiles/Program.cs
@@ -6,14 +6,24 @@ const string filename = "TestFile.txt";
 
 // TODO 1: WriteAllText overwrites a file with the given content
 if (!File.Exists(filename)) {
+    File.WriteAllText(filename, "This is a text file !!");
 
 }
 
 // TODO 3: AppendAllText adds text to an existing file
+File.AppendAllText(filename, "This text gets appended to the file.");
 
 
 // TODO 4: A FileStream can be opened and written to until closed
+using(StreamWriter sw = File.AppendText(filename)){
+    sw.WriteLine("Line1");
+    sw.WriteLine("Line2");
+    sw.WriteLine("Line3");
+}
 
 
 // TODO 2: ReadAllText reads all the content from a file
+string content;
+content = File.ReadAllText(filename);
+System.Console.WriteLine(content);
 
